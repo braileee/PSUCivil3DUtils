daf0760fc5c3bfad53c929b19750ded668a2568e
Stanislav Pavlovitsky <s.pavlovitsky@gmail.com>
2023-08-07 10:44:16 +0200

    update to .net sdk

--- AutoCADUtils/Properties/Settings.Designer.cs
+++ AutoCADUtils/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Civil3DUtils.Properties {
+namespace AutoCADUtils.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
